b9ad5778a5920004b418485486b16533826701b1
Limwonbin <wonbin3366@naver.com>
2023-02-15 15:26:27 +0900

    초기환경셋팅완료

---
+++ src/main/java/shop/mtcoding/wonbin/WonbinApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.wonbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonbinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/wonbin/WonbinApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.wonbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
